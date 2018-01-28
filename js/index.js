861b8110d8ef5457047e3f9e2da322b73cc350c6
Capuccino <chinodesuuu@gmail.com>
2018-01-28 08:31:07 +0000

    add browserchecker to prevent IEFags to navigate here

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+/* global navigator */
 const submitter = document.getElementById('poem-submitter');
 const spinner = document.getElementById('poem-spinner');
 const poemCard = document.getElementById('poem-card');
 const poemImg = document.getElementById('poem-card-img');
 const errCard = document.getElementById('error-card');
+const browser = get_browser();
 const toasts = [];
 
 let font = 'm1';
@@ -23,6 +25,8 @@ submitter.addEventListener('click', function() {
         handleError(err);
     });
 });
+
+console.log(`Detected Browser Version: ${browser.name} ${browser.version}`);
 
 function makeToast(text) {
     // This actually broke Materialize.
@@ -78,3 +82,25 @@ function handleError(err) {
     errCard.classList.remove('hidden');
     document.getElementById('error-text').innerHTML = err.mesasge || err;
 }
+
+/**
+ * Gets the browser version 
+ * @returns {Object}
+ */
+function get_browser() {
+    var ua = navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+    if(/trident/i.test(M[1])){
+        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+        return {name:'IE',version:(tem[1]||'')};
+        }   
+    if(M[1]==='Chrome'){
+        tem=ua.match(/\bOPR|Edge\/(\d+)/);
+        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+        }   
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+    return {
+      name: M[0],
+      version: M[1]
+    };
+ }
