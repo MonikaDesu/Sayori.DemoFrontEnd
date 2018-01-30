0db7fd556e4dea234544d71b8ba873a936298238
Ovyerus <iamovyerus@gmail.com>
2018-01-30 12:40:25 +1100

    Merge

--- js/browserChecker.js
+++ js/browserChecker.js
@@ -1,46 +1,23 @@
-/* global navigator */
-/* global M */
-
-/**
- * Gets the browser version 
- * @returns {Object}
- */
-function get_browser() {
-    var ua = navigator.userAgent,
-        tem, M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if (/trident/i.test(M[1])) {
-        tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
-        return { name: 'IE', version: (tem[1] || '') };
-    }
-    if (M[1] === 'Chrome') {
-        tem = ua.match(/\bOPR|Edge\/(\d+)/);
-        if (tem != null) { return { name: 'Opera', version: tem[1] }; }
-    }
-    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
-    if ((tem = ua.match(/version\/(\d+)/i)) != null) { M.splice(1, 1, tem[1]); }
-    return {
-        name: M[0],
-        version: M[1]
-    };
-}
-
+var a = function() {
+  try {
+    new Function("(a = 0) => a");
+    return true;
+  }
+  catch (err) {
+    return false;
+  }
+}();
 
 var elem = document.getElementById('unsupported-modal');
 var instance = M.Modal.init(elem, {
     dismissible: false
 });
 
-var acceptedVersions = [
-    "Chrome59",
-    "Chrome61",
-    "Chrome62",
-    "Opera15",
-    "Firefox57",
-    "Firefox58",
-    "Firefox59"
-];
-
-//check browser version if in whitelist. If it doesn't exist
-if (get_browser().name + get_browser().version !== acceptedVersions) {
-    instance.open();
+if (a) {
+  var script = document.createElement("script");
+  script.src = "my-es6-file.js";
+  document.head.appendChild(script);
+  console.log('Browser supports ECMAScript 6.');
+} else {
+     instance.open();
 }
--- js/index.js
+++ js/index.js
@@ -64,27 +64,6 @@ function handleError(err) {
     document.getElementById('error-text').innerHTML = err.mesasge || err;
 }
 
-function getBrowser() {
-    var ua = navigator.userAgent,
-        tem, M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if (/trident/i.test(M[1])) {
-        tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
-        return { name: 'IE', version: (tem[1] || '') };
-    }
-    if (M[1] === 'Chrome') {
-        tem = ua.match(/\bOPR|Edge\/(\d+)/);
-        if (tem != null) { return { name: 'Opera', version: tem[1] }; }
-    }
-    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
-    if ((tem = ua.match(/version\/(\d+)/i)) != null) { M.splice(1, 1, tem[1]); }
-    return {
-        name: M[0],
-        version: M[1]
-    };
-}
-
-const browser = getBrowser();
-
 M.Dropdown.init(document.getElementById('poem-dropdown'));
 submitter.addEventListener('click', function() {
     this.classList.add('disabled');
@@ -101,5 +80,3 @@ submitter.addEventListener('click', function() {
         handleError(err);
     });
 });
-
-console.log(`Detected Browser Version: ${browser.name} ${browser.version} \nInclude this output on when you report an issue in GitHub.`);
